e752ce45b492bf6a30bd6e2e2277619b774dd0b7
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-02-16 15:52:32 +0000

    A function that sorts an array of integers in ascending order using the Bubble sort algorithm: print the array after each time you swap two elements

--- sort.h
+++ sort.h
@@ -19,7 +19,7 @@ typedef struct listint_s
 } listint_t;
 
 /* Printing helper functions */
-void print_array(const int *array, size_t size;)
+void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 /* Sorting Algorithms Prototypes */
